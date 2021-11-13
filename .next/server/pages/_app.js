b8f3f7f8f77e9ad76c1a2616e246bc5819319acf
anore <66754841+anoredev@users.noreply.github.com>
2021-11-13 15:12:54 +0300

    major: rebuilded to default next.js typescript application

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -5,102 +5,31 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 29:
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+/***/ 656:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
 
 
-Object.defineProperty(exports, "__esModule", ({
-    value: true
-}));
-Object.defineProperty(exports, "AppInitialProps", ({
-    enumerable: true,
-    get: function() {
-        return _utils.AppInitialProps;
-    }
-}));
-Object.defineProperty(exports, "NextWebVitalsMetric", ({
-    enumerable: true,
-    get: function() {
-        return _utils.NextWebVitalsMetric;
-    }
-}));
-exports["default"] = void 0;
-var _react = _interopRequireDefault(__webpack_require__(689));
-var _utils = __webpack_require__(232);
-function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-    try {
-        var info = gen[key](arg);
-        var value = info.value;
-    } catch (error) {
-        reject(error);
-        return;
-    }
-    if (info.done) {
-        resolve(value);
-    } else {
-        Promise.resolve(value).then(_next, _throw);
-    }
+function MyApp({ Component , pageProps  }) {
+    return(/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+        ...pageProps
+    }));
 }
-function _asyncToGenerator(fn) {
-    return function() {
-        var self = this, args = arguments;
-        return new Promise(function(resolve, reject) {
-            var gen = fn.apply(self, args);
-            function _next(value) {
-                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-            }
-            function _throw(err) {
-                asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-            }
-            _next(undefined);
-        });
-    };
-}
-function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-        default: obj
-    };
-}
-function _appGetInitialProps() {
-    _appGetInitialProps = /**
- * `App` component is used for initialize of pages. It allows for overwriting and full control of the `page` initialization.
- * This allows for keeping state between navigation, custom error handling, injecting additional data.
- */ _asyncToGenerator(function*({ Component , ctx  }) {
-        const pageProps = yield (0, _utils).loadGetInitialProps(Component, ctx);
-        return {
-            pageProps
-        };
-    });
-    return _appGetInitialProps.apply(this, arguments);
-}
-function appGetInitialProps(_) {
-    return _appGetInitialProps.apply(this, arguments);
-}
-class App extends _react.default.Component {
-    render() {
-        const { Component , pageProps  } = this.props;
-        return(/*#__PURE__*/ _react.default.createElement(Component, Object.assign({
-        }, pageProps)));
-    }
-}
-App.origGetInitialProps = appGetInitialProps;
-App.getInitialProps = appGetInitialProps;
-exports["default"] = App; //# sourceMappingURL=_app.js.map
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MyApp);
 
 
 /***/ }),
 
-/***/ 232:
+/***/ 997:
 /***/ ((module) => {
 
-module.exports = require("next/dist/shared/lib/utils.js");
-
-/***/ }),
-
-/***/ 689:
-/***/ ((module) => {
-
-module.exports = require("react");
+module.exports = require("react/jsx-runtime");
 
 /***/ })
 
@@ -111,7 +40,7 @@ module.exports = require("react");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(29));
+var __webpack_exports__ = (__webpack_exec__(656));
 module.exports = __webpack_exports__;
 
 })();
